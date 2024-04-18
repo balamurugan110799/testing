e7c3c5408cf281e87f3602b97653c3873615a6a6
balamurugan110799 <balamurugan99711@gmail.com>
2024-04-18 09:24:34 +0530

    updates\

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import logo from "./logo.svg";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 
 import "./App.css";
 import Home from "./Home";
 import Hello from "./Hello";
+import NotFound from "./NotFound";
 
 function App() {
+
+  console.log(window.location)
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/hello" element={<Hello />} />
+          <Route path="*" element={<NotFound />} />
+
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,10 @@
 import React from 'react'
-
+import Flight from "./assets/flight.png"
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>Home
+
+        <img src={Flight} alt="Flight" />
+    </div>
   )
 }
